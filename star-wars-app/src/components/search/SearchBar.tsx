41207ce2c1e4a95c219d06d23767a91bf40451a6
jmestanza <jmestanza@itba.edu.ar>
2023-09-15 02:54:25 -0300

    updated style for search bar

--- star-wars-app/src/components/search/SearchBar.tsx
+++ star-wars-app/src/components/search/SearchBar.tsx
@@ -7,11 +7,8 @@ const SearchBar = (props: any) => {
     props.onSearch();
   };
   return (
-    <>
-      <form>
-        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
-          Search
-        </label>
+    <div className="flex justify-center items-center">
+      <div className="w-3/4 flex justify-end">
         <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg
@@ -34,20 +31,13 @@ const SearchBar = (props: any) => {
             onChange={searchNameHandler}
             type="search"
             id="default-search"
-            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search Name"
+            className="p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-yellow-500 bg-black dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-500 dark:focus:border-yellow-500"
+            placeholder="Search character"
             required
           />
-          {/* <button
-            onClick={() => props.onSearch()}
-            type="submit"
-            className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Search
-          </button> */}
         </div>
-      </form>
-    </>
+      </div>
+    </div>
   );
 };
 
