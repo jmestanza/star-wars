b78af13f4bb820ae7be71f334c6b0625de7ad075
jmestanza <jmestanza@itba.edu.ar>
2023-09-15 18:03:53 -0300

    added slug

--- star-wars-app/src/components/character/CharacterCard.tsx
+++ star-wars-app/src/components/character/CharacterCard.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Character from "../../../models/character.dto";
 import Image from "next/image";
 
@@ -9,12 +10,15 @@ const CharacterCard = ({ character }: CharacterProps) => {
   return (
     <div className="flex items-center flex-col group opacity-80 overflow-hidden m-5 border-solid border-2 border-stone-600 rounded hover:border-yellow-500 hover:opacity-100 bg-black">
       <div className="grayscale group-hover:grayscale-0 relative bg-gray-50/75 cursor-pointer group-hover:scale-110 transition duration-300 ease-in-out">
-        <Image
-          src={`/img/people/${character.id}.jpg`}
-          width="430"
-          height="200"
-          alt="name"
-        />
+        <Link href={`/characters/${character.id}`}>
+          <Image
+            src={`/img/people/${character.id}.jpg`}
+            width="430"
+            height="200"
+            alt="name"
+          />
+        </Link>
+
         <div className="absolute inset-y-2/4 right-5 group-hover:opacity-100">
           <h1 className="text-white font-semibold text-base font-primary">
             {character.name}
